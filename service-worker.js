d021affad0abc18549e46a907c3ab0174e3f6f80
adriasc <42960713+adriasc@users.noreply.github.com>
2026-02-17 22:53:59 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cantonese-coach-v1";
+const CACHE_NAME = "cantonese-coach-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -11,12 +11,14 @@ const ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
