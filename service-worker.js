7ca73d856f00491b50025a05068260b1ec4ea4f5
adriasc <42960713+adriasc@users.noreply.github.com>
2026-02-19 18:17:32 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cantonese-coach-v16";
+const CACHE_NAME = "cantonese-coach-v17";
 const ASSETS = [
   "./",
   "./index.html",
@@ -22,7 +22,22 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  const isAppAsset = ASSETS.some((asset) => url.pathname.endsWith(asset.replace("./", "/")));
+
+  if (isAppAsset || event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
